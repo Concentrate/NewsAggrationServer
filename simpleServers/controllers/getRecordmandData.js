0c09cb09cf73aabebdee880f7914f681ccfdbf86
liudeyu <liudeyu@bytedance.com>
2018-04-12 12:48:47 +0800

    修复js array用法问题

--- simpleServers/controllers/getRecordmandData.js
+++ simpleServers/controllers/getRecordmandData.js
@@ -47,12 +47,16 @@ async function getRecomendData(ctx, next) {
     }
     var resultData = []
     if (recommendDatas && recommendDatas.length > 0) {
-        resultData.push(recommendDatas)
+        recommendDatas.forEach(item => {
+            resultData.push(item)
+        })
     }
     if (resultData.length < every_page_num) {
         var leftNum = every_page_num - resultData.length
         if (allData && (page + 1) * leftNum < allData.length) {
-            resultData.push(allData.slice(page * leftNum, page * leftNum + leftNum))
+            allData.slice(page * leftNum, page * leftNum + leftNum).forEach(item => {
+                resultData.push(item)
+            })
         }
     } else {
         resultData = resultData.splice(0, every_page_num)
